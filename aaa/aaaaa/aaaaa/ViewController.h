98e057dc966ea80207a674abf5060f0e42b0776e
15251918299 <15251918299@sina.cn>
2016-06-02 15:17:42 +0800

    Add files via upload

---
+++ aaa/aaaaa/aaaaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaaaa
+//
+//  Created by Chuanxi.Chen on 16/6/2.
+//  Copyright © 2016年 Chuanxi.Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ aaa/aaaaa/aaaaa/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  aaaaa
+//
+//  Created by Chuanxi.Chen on 16/6/2.
+//  Copyright © 2016年 Chuanxi.Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        NSLog(@"aaaaa");
+        NSLog(@"bbb`");
+
+        NSLog(@"12312312313");
+
+    }
+}
